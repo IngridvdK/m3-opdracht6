d04a3893cec2227fea49bac83742c59ce402bfa4
IngridvdK <79993280+IngridvdK@users.noreply.github.com>
2021-05-22 21:03:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("werkt mijn script document?");
+const bakePizza = function () {
+    console.log("Warm de oven op.");
+    console.log("Maak het deeg.");
+    console.log("Snij de groeten.");
+    console.log("Maak de saus.");
+    console.log("Zet de pizza in elkaar.");
+    console.log("Doe de pizza in de oven.");
+};
+
+bakePizza();
+bakePizza();
+
+const makeSushi = function () {
+    console.log("Kook de rijst.");
+    console.log("Snij de vis.");
+    console.log("Rol de ingrediënten.");
+    console.log("Snij de rol in stukken.");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
